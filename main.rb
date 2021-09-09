47af5a2ba89536fb0bd5c5675e3c99e4db4a6d5f
Taro Morita <morita0079@gmail.com>
2021-09-10 02:01:21 +0900

    add some functions

--- main.rb
+++ main.rb
@@ -1,21 +1,20 @@
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/poltergeist'
-require 'nokogiri'
+# require 'nokogiri'
 
 class Scraping
-  include Capybara::DSL
   attr_reader :doc, :page, :xpaths
-
 
   def initialize(url)
     Capybara.register_driver :poltergeist do |app|
-      Capybara::Poltergeist::Driver.new(app, :inspector => true )
+      Capybara::Poltergeist::Driver.new(app, {inspector: true, js_errors: false})
     end
-    Capybara.default_driver = :poltergeist
     Capybara.javascript_driver = :poltergeist
-    # Capybara.default_selector = :xpath
     @page = Capybara::Session.new(:poltergeist)
+    page.driver.headers = {
+      'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2564.97 Safari/537.36'
+    }
     page.visit(url)
 
     @xpaths = {
@@ -24,12 +23,11 @@ class Scraping
       floor: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[3]/td',
       madori: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[4]/td',
       sikikin_and_reikin: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[6]/td',
-
+      tyukai_tesuryo: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[7]/td',
+      hosyokin: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[8]/td',
 
     }
-
   end
-
 
   def allpoperties
     {
@@ -37,10 +35,15 @@ class Scraping
       management_fee: management_fee,
       floor: floor,
       madori: madori,
-      sikikin_and_reikin: sikikin_and_reikin
+      sikikin_and_reikin: sikikin_and_reikin,
+      tyukai_tesuryo: tyukai_tesuryo,
+      hosyokin: hosyokin
+
     }
   end
+
   private
+
   def scraping_data(target)
     page.find(:xpath, target).text
   end
@@ -62,11 +65,17 @@ class Scraping
   end
 
   def sikikin_and_reikin
-    scraping_data(sikikin_and_reikin)
+    scraping_data(xpaths[:sikikin_and_reikin])
   end
 
+  def tyukai_tesuryo
+    scraping_data(xpaths[:tyukai_tesuryo])
+  end
 
+  def hosyokin
+    scraping_data(xpaths[:hosyokin])
+  end
 end
 
-sc = Scraping.new('https://www.goodrooms.jp/tokyo/detail/001/5240702/?transit_area=216')
+sc = Scraping.new('https://www.goodrooms.jp/tokyo/detail/001/5240702/?transit_area=216%27')
 p sc.allpoperties
