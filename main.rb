bb1d33ab577f64eae84a78914216853daa582ba4
Taro Morita <morita0079@gmail.com>
2021-09-10 00:10:54 +0900

    add some function

--- main.rb
+++ main.rb
@@ -21,6 +21,11 @@ class Scraping
     @xpaths = {
       rent_fee: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[1]/td',
       management_fee: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[2]/td',
+      floor: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[3]/td',
+      madori: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[4]/td',
+      sikikin_and_reikin: '/html/body/div[1]/main/article/div[2]/div[2]/table/tbody/tr[6]/td',
+
+
     }
 
   end
@@ -29,7 +34,10 @@ class Scraping
   def allpoperties
     {
       rent_fee: rent_fee,
-      management_fee: management_fee
+      management_fee: management_fee,
+      floor: floor,
+      madori: madori,
+      sikikin_and_reikin: sikikin_and_reikin
     }
   end
   private
@@ -43,6 +51,18 @@ class Scraping
 
   def management_fee
     scraping_data(xpaths[:management_fee])
+  end
+
+  def floor
+    scraping_data(xpaths[:floor])
+  end
+
+  def madori
+    scraping_data(xpaths[:madori])
+  end
+
+  def sikikin_and_reikin
+    scraping_data(sikikin_and_reikin)
   end
 
 
